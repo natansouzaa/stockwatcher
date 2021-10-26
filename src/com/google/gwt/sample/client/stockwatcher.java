935e2a003e97bf86f9a0c69615c0543101eccd9e
natansouzaa <natan.souza@ccc.ufcg.edu.br>
2021-10-26 16:50:33 -0300

    Add all interface events

--- src/com/google/gwt/sample/client/stockwatcher.java
+++ src/com/google/gwt/sample/client/stockwatcher.java
@@ -6,6 +6,10 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.i18n.client.NumberFormat;
+import com.google.gwt.user.client.Random;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -14,15 +18,19 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.Window;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class stockwatcher implements EntryPoint {
 
+  private static final int REFRESH_INTERVAL = 5000; // ms
   private VerticalPanel mainPanel = new VerticalPanel();
   private FlexTable stocksFlexTable = new FlexTable();
   private HorizontalPanel addPanel = new HorizontalPanel();
   private TextBox newSymbolTextBox = new TextBox();
   private Button addStockButton = new Button("Add");
   private Label lastUpdatedLabel = new Label();
+  private ArrayList<String> stocks = new ArrayList<String>();
 
   /**
    * Entry point method.
@@ -48,6 +56,15 @@ public class stockwatcher implements EntryPoint {
 
     // Move cursor focus to the input box.
     newSymbolTextBox.setFocus(true);
+
+    // Setup timer to refresh list automatically.
+    Timer refreshTimer = new Timer() {
+      @Override
+      public void run() {
+        refreshWatchList();
+      }
+    };
+    refreshTimer.scheduleRepeating(REFRESH_INTERVAL);
 
     // Listen for mouse events on the Add button.
     addStockButton.addClickHandler(new ClickHandler() {
@@ -83,10 +100,91 @@ public class stockwatcher implements EntryPoint {
 
       newSymbolTextBox.setText("");
 
-      // TODO Don't add the stock if it's already in the table.
-      // TODO Add the stock to the table
-      // TODO Add a button to remove this stock from the table.
-      // TODO Get the stock price.
+      // Don't add the stock if it's already in the table.
+      if (stocks.contains(symbol)) {
+        return;
+      }
+
+      // Add the stock to the table
+      int row = stocksFlexTable.getRowCount();
+      stocks.add(symbol);
+      stocksFlexTable.setText(row, 0, symbol);
+
+      // Add a button to remove this stock from the table.
+      Button removeStockButton = new Button("x");
+      removeStockButton.addClickHandler(new ClickHandler() {
+        public void onClick(ClickEvent event) {
+          int removedIndex = stocks.indexOf(symbol);
+          stocks.remove(removedIndex);
+          stocksFlexTable.removeRow(removedIndex + 1);
+        }
+      });
+      stocksFlexTable.setWidget(row, 3, removeStockButton);
+
+      // Get the stock price.
+      refreshWatchList();
   }
 
+  /**
+    * Generate random stock prices.
+    */
+  private void refreshWatchList() {
+    final double MAX_PRICE = 100.0; // $100.00
+    final double MAX_PRICE_CHANGE = 0.02; // +/- 2%
+
+    StockPrice[] prices = new StockPrice[stocks.size()];
+    for (int i = 0; i < stocks.size(); i++) {
+      double price = Random.nextDouble() * MAX_PRICE;
+      double change = price * MAX_PRICE_CHANGE
+          * (Random.nextDouble() * 2.0 - 1.0);
+
+      prices[i] = new StockPrice(stocks.get(i), price, change);
+    }
+
+    updateTable(prices);
+  }
+
+  /**
+    * Update the Price and Change fields all the rows in the stock table.
+    *
+    * @param prices
+    *          Stock data for all rows.
+    */
+  private void updateTable(StockPrice[] prices) {
+    for (int i = 0; i < prices.length; i++) {
+      updateTable(prices[i]);
+    }
+
+    // Display timestamp showing last refresh.
+    DateTimeFormat dateFormat = DateTimeFormat.getFormat(
+      DateTimeFormat.PredefinedFormat.DATE_TIME_MEDIUM);
+    lastUpdatedLabel.setText("Last update : " 
+      + dateFormat.format(new Date()));
+  }
+
+  /**
+    * Update a single row in the stock table.
+    *
+    * @param price Stock data for a single row.
+    */
+  private void updateTable(StockPrice price) {
+    // Make sure the stock is still in the stock table.
+    if (!stocks.contains(price.getSymbol())) {
+      return;
+    }
+
+    int row = stocks.indexOf(price.getSymbol()) + 1;
+
+    // Format the data in the Price and Change fields.
+    String priceText = NumberFormat.getFormat("#,##0.00").format(
+        price.getPrice());
+    NumberFormat changeFormat = NumberFormat.getFormat("+#,##0.00;-#,##0.00");
+    String changeText = changeFormat.format(price.getChange());
+    String changePercentText = changeFormat.format(price.getChangePercent());
+
+    // Populate the Price and Change fields with new data.
+    stocksFlexTable.setText(row, 1, priceText);
+    stocksFlexTable.setText(row, 2, changeText + " (" + changePercentText
+        + "%)");
+  }
 }
